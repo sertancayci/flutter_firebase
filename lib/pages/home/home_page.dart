3bb095b0a14f92363f29a7bdfece105d79282b32
sertancayci <sertancayci@gmail.com>
2023-09-28 23:56:39 +0300

    notif wip

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,7 +1,8 @@
-
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/services/firebase_api.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,7 +12,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   FirebaseApi notifacationServices = FirebaseApi();
 
   @override
@@ -20,14 +20,38 @@ class _HomePageState extends State<HomePage> {
     notifacationServices.refreshToken();
     notifacationServices.initPushNotifications(context);
     notifacationServices.setupInteractMessage(context);
-
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Home Page')),
+      body: Center(
+        child: TextButton(
+            onPressed: () {
+              notifacationServices.getDeviceToken().then((value) async {
+                var data = {
+                  "to": value,
+                  "notification": {
+                    "title": "Flutter Firebase",
+                    "body": "Flutter Firebase Cloud Messaging",
+                    "mutable_content": true,
+                    "sound": "Tri-tone"
+                  },
+                  "data": {
+                    "type": "msj",
+                    "id": "1",
+                  }
+                };
+                await http.post(Uri.parse(uri),
+                    body: jsonEncode(data),
+                    headers: {
+                    'Content-Type': 'applicatiom/json; charset=UTF-8',
+                      'Authorization': 'key=$serverToken',
+                });
+              });
+            },
+            child: Text('Send Notification')),
+      ),
     );
   }
 }
